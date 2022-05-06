6f6f79fce655793122fced16b1f16e1f309fd9d8
newjini <kimyjin0920@gmail.com>
2022-05-07 00:31:18 +0900

    code: BOJ1107_리모컨

--- Java/src/bruteForth/BOJ1107_리모컨.java
+++ Java/src/bruteForth/BOJ1107_리모컨.java
@@ -6,53 +6,35 @@ import java.util.*;
 public class BOJ1107_리모컨 {
 
     private static String N;
-    private static String now = "100";
+    private static String first = "100";
     private static boolean[] nums;
-    private static int cnt;
 
+    static int solve() {
+        int cnt = 0;
 
-    static void solve() {
-        int leng = N.length();
-        if(now.equals(N)){
-            cnt = 0;
-            return;
+        if(first.equals(N)){
+            return cnt;
         }
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < leng; i++) {
-            int btn = N.charAt(i) - '0';
-            if(!nums[btn]){
-                sb.append(btn);
-            }
-            else{
-                System.out.println(btn);
-                int num1 = btn;
-                int num2 = btn;
+        int cal = 0;
+        int min = Math.abs(Integer.parseInt(first) - Integer.parseInt(N));
 
-                while(num1 > 0) {
-                    num1--;
-                    if(!nums[num1]){
-                        System.out.println("num1 : "+num1);
-                        break;
-                    }
-                }
-                while(num2 < 9){
-                    num2++;
-                    System.out.println("num2 : "+num2);
-                    if(!nums[num2]) {
-                        break;
-                    }
-                }
-                if(num1 == 0){
-                    sb.append(num2);
-                }else if(num2 == 0){
-                    sb.append(num1);
-                }else{
-                    sb.append(Math.min(num1, num2));
+        for (int i = 0; i < 10000000; i++) {
+            boolean check = false;
+            String now = Integer.toString(i);
+            int leng = now.length();
+            for (int j = 0; j < leng; j++) {
+                if(nums[now.charAt(j) - '0']){
+                    check = true;
+                    break;
                 }
             }
+            if(!check){
+                cal = Math.abs(i - Integer.parseInt(N)) + Integer.toString(i).length();
+                min = Math.min(min, cal);
+            }
         }
-        System.out.println(sb);
-        cnt = sb.length() + Math.abs(Integer.parseInt(String.valueOf(sb)) - Integer.parseInt(N));
+        cnt = min;
+        return cnt;
 
     }
 
@@ -68,8 +50,7 @@ public class BOJ1107_리모컨 {
                 nums[Integer.parseInt(st.nextToken())] = true;
             }
         }
-        solve();
-        System.out.println(cnt);
+        System.out.println(solve());
     }
 
     private static BufferedReader getBufferedReader() throws IOException {
